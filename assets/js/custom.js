3b4865f16f70c9e358a00ee77829b77e59ac3b11
tronzed <106952970+tronzed@users.noreply.github.com>
2023-08-22 18:14:24 +0530

    commit 2
    commit 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -69,3 +69,27 @@ $('#trending_products_carousel').owlCarousel({
         }
     }
 });
+
+
+$('#shop_brands_carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:1,
+    dots:0,
+    autoplay:1,
+    autoplayTimeout:5000,
+    autoplaySpeed:2000,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+});
